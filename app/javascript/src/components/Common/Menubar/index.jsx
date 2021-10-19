adf1a6abdc1f68c9517c54579433c78979208401
vysakh-m <vysakhmurali4@gmail.com>
2021-10-19 09:16:42 +0530

    Added MenuBar

---
+++ app/javascript/src/components/Common/Menubar/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import { Search, Settings, Plus } from "@bigbinary/neeto-icons";
+import { Typography } from "neetoui/v2";
+import { MenuBar } from "neetoui/v2/layouts";
+
+const Menubar = () => {
+  const [isSearchCollapsed, setIsSearchCollapsed] = useState(true);
+
+  return (
+    <div className="flex">
+      <MenuBar showMenu={true} title="Contacts">
+        <MenuBar.Block label="All" count={200} active />
+        <MenuBar.Block label="Users" count={80} />
+        <MenuBar.Block label="Leads" count={60} />
+        <MenuBar.Block label="Visitors" count={60} />
+
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: () => <Search size={20} />,
+              onClick: () => setIsSearchCollapsed(!isSearchCollapsed)
+            }
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Segments
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Search
+          collapse={isSearchCollapsed}
+          onCollapse={() => setIsSearchCollapsed(true)}
+        />
+        <MenuBar.Block label="Europe" count={80} />
+        <MenuBar.Block label="Middle-East" count={60} />
+        <MenuBar.Block label="Asia" count={60} />
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: () => <Settings size={20} />
+            },
+            {
+              icon: () => <Plus size={20} />
+            },
+            {
+              icon: () => <Search size={20} />
+            }
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Tags
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Block label="Sales" count={80} />
+        <MenuBar.Block label="Finance" count={60} />
+        <MenuBar.Block label="User Experience" count={60} />
+      </MenuBar>
+    </div>
+  );
+};
+
+export default Menubar;
--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Route, Redirect, Switch } from "react-router-dom";
 
+import Menubar from "components/Common/Menubar";
 import Navbar from "components/Common/Navbar";
 
 import PasswordEdit from "./Account/Passwords/Edit";
@@ -12,9 +13,11 @@ const Home = () => {
   return (
     <div className="flex h-screen">
       <Navbar />
+      <Menubar />
       <div className="flex flex-col items-start justify-start flex-grow h-screen overflow-y-auto">
         <Switch>
           <Route exact path="/notes" component={Notes} />
+          {/* <Route exact path="/contacts" component={Notes} /> */}
           <Route exact path="/my/password/edit" component={PasswordEdit} />
           <Route exact path="/my/profile" component={Profile} />
           <Redirect from="/" to="/notes" />
