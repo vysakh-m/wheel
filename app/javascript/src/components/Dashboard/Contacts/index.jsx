4c02a358a0765ee98f0edb42fa2ce60eb6e6a256
vysakh-m <vysakhmurali4@gmail.com>
2021-10-20 11:28:38 +0530

    Added Menubar & Header

--- app/javascript/src/components/Dashboard/Contacts/index.jsx
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import CustomHeader from "components/Common/CustomHeader";
+import Menubar from "components/Common/Menubar";
 
 const Contacts = () => {
-  return <></>;
+  const [showMenu, setShowMenu] = useState(true);
+  const [searchContent, setSearchContent] = useState("");
+  // const [showPane, setShowPane] = useState(false);
+  //Dummy Function to bypass eslint
+  let setShowPane = () => {};
+
+  return (
+    <>
+      <Menubar showMenu={showMenu} title="Contacts" />
+
+      <div className="flex flex-col items-start justify-start flex-grow h-screen overflow-y-auto mr-7 ml-5">
+        <CustomHeader
+          searchContent={searchContent}
+          setSearchContent={setSearchContent}
+          setShowPane={setShowPane}
+          setShowMenu={setShowMenu}
+          showMenu={showMenu}
+          buttonLabel="Add Contact +"
+          title="All Contacts"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Contacts;
